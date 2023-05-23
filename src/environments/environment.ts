6111ba30117608fea2e6707347759931fa1586a0
jhoanjimz <jhoan.developer@gmail.com>
2023-05-23 13:29:23 -0500

    environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
     api: 'https://apibuentaxi.somosinnlab.co/api',
-    apiImg: 'https://api.sictaxi.gov.co/imagenes/conductores/'
+    apiImg: 'https://api.sictaxi.gov.co/imagenes/conductores/' 
 };
