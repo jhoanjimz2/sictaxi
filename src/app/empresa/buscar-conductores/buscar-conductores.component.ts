8ba9a2d44f084dc626e98b7f1f5c7f71bc19a5e7
jhoanjimz <jhoan.developer@gmail.com>
2023-07-05 21:22:33 -0500

    ajuste de tarjeton

--- src/app/empresa/buscar-conductores/buscar-conductores.component.ts
+++ src/app/empresa/buscar-conductores/buscar-conductores.component.ts
@@ -96,130 +96,138 @@ export class BuscarConductoresComponent {
 
     var img = new Image();
     var img2 = new Image();
-    var factorx =0;
-    var factory =0;
-    var printer=data.tipoImpresion;  
-    var numerocon=(4-data.consecutivo.length);
-    var ceros='';
-    if(numerocon>0){
-      for(var i=0;i<numerocon;i++){
-        ceros+="0";  
+    var factorx = 0;
+    var factory = 0;
+    var printer = data.tipoImpresion;
+    var numerocon = (4 - data.consecutivo.length);
+    var units = data.units;
+    var ceros = '';
+    if (numerocon > 0) {
+      for (var i = 0; i < numerocon; i++) {
+        ceros += "0";
       }
-    }      
-    ceros+=data.consecutivo;
-
-    if(data.tipoImpresion==2){
-      factorx=2.5;
-      factory=0;
-    }else{
-      factorx=0;
-      factory=0;
     }
+    ceros = data.datos.idMatricula;
 
-    if(data.imagen === null || data.imagen===''){
+
+    if (data.imagen === null || data.imagen === '') {
       img.src = '../assets/img/user.svg';
-    }else{
+    } else {
       img.src = data.imagen;
     }
-    img.style.height ="181px";
-    img.style.width ="183px";
-    img.crossOrigin ="Anonymous";
-    if(data.imagenEmp === null || data.imagenEmp===''){
+    img.style.height = "181px";
+    img.style.width = "183px";
+    img.crossOrigin = "Anonymous";
+    if (data.imagenEmp === null || data.imagenEmp === '') {
       img2.src = '../assets/img/company.svg';
-    }else{
+    } else {
       img2.src = data.imagenEmp;
     }
-    img2.style.height ="181px";
-    img2.style.width ="183px";
-    img2.crossOrigin ="Anonymous";    
+    img2.style.height = "181px";
+    img2.style.width = "183px";
+    img2.crossOrigin = "Anonymous";
 
-    img.onload=()=>{
-      img.crossOrigin ="Anonymous";            
-      var canvas = <HTMLCanvasElement> document.createElement('canvas');
+    img.onload = () => {
+      img.crossOrigin = "Anonymous";
+      var canvas = <HTMLCanvasElement>document.createElement('canvas');
       var context = canvas.getContext('2d');
       context!.fillStyle = '#fff'
-      context!.drawImage(img,0,0);
-      var dataURL = canvas.toDataURL("image/png");        
+      context!.drawImage(img, 0, 0);
+      var dataURL = canvas.toDataURL("image/png");
       document.body.appendChild(img);
 
-      img2.crossOrigin ="Anonymous";            
-      var canvas2 = <HTMLCanvasElement> document.createElement('canvas');
+      img2.crossOrigin = "Anonymous";
+      var canvas2 = <HTMLCanvasElement>document.createElement('canvas');
       var context2 = canvas2.getContext('2d');
       context2!.fillStyle = '#fff'
-      context2!.drawImage(img2,0,0);
-      var dataURL = canvas2.toDataURL("image/png");        
+      context2!.drawImage(img2, 0, 0);
+      var dataURL = canvas2.toDataURL("image/png");
       document.body.appendChild(img2);
 
-      var doc = new jsPDF('landscape','cm','legal');
+      var doc = new jsPDF('landscape', 'cm', 'legal');
       doc.setFont("TimesNewRomanPS", "", "bold");
 
       doc.setFontSize(11);
-      doc.addImage(img,'PNG',1+factorx,3.4+factory,5.3,6.7); //FOTO DEL CONDUCTOR
-      img.remove();   
-      doc.setFontSize(12);                    
-      doc.text(ceros, 16.6+factorx, 2.79+factory);        
-      doc.setFontSize(11);
-      doc.text(data.datos.fechaLicenciaConduccion, 15+factorx, 5+factory);    
+      doc.addImage(img, 'PNG', units.foto.x + factorx, units.foto.y + factory, 5.3, 6.7); //FOTO DEL CONDUCTOR
+      img.remove();
+      doc.setFontSize(12);
+      doc.text(ceros, units.censo.x + factorx, units.censo.y + factory); //Número de censo
+
+
+      //MODIFICADO
       doc.setFontSize(10);
-      doc.text(data.datos.nombres+' '+data.datos.apellidos, 10.6+factorx, 4.38+factory);
+      doc.text(data.datos.nombres + ' ' + data.datos.apellidos, units.nombres.x + factorx, units.nombres.y + factory);
       doc.setFontSize(11);
-      doc.text(data.datos.licenciaConduccion, 10.6+factorx, 5+factory);
+      doc.text(units.licencia.x + factorx, units.licencia.y + factory, data.datos.licenciaConduccion);
+      doc.text(units.fechaLicenciaConduccion.x + factorx, units.fechaLicenciaConduccion.y + factory, data.datos.fechaLicenciaConduccion);
       doc.setFontSize(12);
-      doc.text(data.datos.categoriaPase, 10.6+factorx, 5.75+factory);   
-      if(data.Eps!==null){
-        doc.setFontSize(10);  
-        doc.text(data.Eps.nombre, 8+factorx, 6.5+factory);   
-      }   
-      if(data.Arl!==null){
-        doc.setFontSize(6);  
-        doc.text(data.Arl.nombre, 15+factorx, 6.4+factory);   
-      }    
-      if(data.Afp!==null){
-        doc.setFontSize(10);  
-        doc.text(data.Afp.nombre, 10.6+factorx, 7.18+factory);   
-      }     
+      doc.text(units.categoriaPase.x + factorx, units.categoriaPase.y + factory, data.datos.categoriaPase);
+      if (data.Eps !== null) {
+        doc.setFontSize(10);
+        doc.text(units.eps.x + factorx, units.eps.y + factory, data.Eps.nombre);
+      }
+      if (data.Arl !== null) {
+        doc.setFontSize(6);
+        doc.text(units.arl.x + factorx, units.arl.y + factory, data.Arl.nombre);
+      }
+      if (data.Afp !== null) {
+        doc.setFontSize(10);
+        doc.text(units.afp.x + factorx, units.afp.y + factory, data.Afp.nombre);
+      }
       doc.setFontSize(12);
-      doc.text(data.datos.tipoSangre, 10.6+factorx, 7.8+factory);
-      doc.text(data.datos.rh, 15+factorx, 7.8+factory); 
+      doc.text(units.tipoSangre.x + factorx, units.tipoSangre.y + factory, data.datos.tipoSangre);
+      doc.text(units.rh.x + factorx, units.rh.y + factory, data.datos.rh);
+
+
+      //SIN MODIFICAR
       doc.setFontSize(8);
-      doc.text(data.datos.razonSocial, 10.6+factorx, 9.6+factory);        
-      doc.text(data.datos.nit, 10.6+factorx, 10.3+factory);
-      doc.text(data.datos.direccione, 10.6+factorx, 11.1+factory);    
-      doc.text(data.datos.tele, 10.6+factorx, 11.8+factory);
+      doc.text(units.razonSocial.x + factorx, units.razonSocial.y + factory, data.datos.razonSocial);
+      doc.text(units.nit.x + factorx, units.nit.y + factory, data.datos.nit);
+      doc.text(units.direccion.x + factorx, units.direccion.y + factory, data.datos.direccione);
+      doc.text(units.telefono.x + factorx, units.telefono.y + factory, data.datos.tele);
       doc.setFontSize(6);
-      doc.text(data.datos.emaile, 10.6+factorx, 12.3+factory);
+      doc.text(units.email.x + factorx, units.email.y + factory, data.datos.emaile);
+      doc.addImage(img2, 'PNG', units.logo.x + factorx, units.logo.y + factory, 2, 2.8); //LOGO DE LA EMPRESA
+      img2.remove();
 
-      doc.addImage(img2,'PNG',15+factorx,9.7+factory,2,2.8); //LOGO DE LA EMPRESA
-      img2.remove();   
-
+      //MODIFICADO
       doc.setFontSize(35);
-      doc.text(data.datos.placa, 0.8+factorx,13.68+factory)
+      doc.text(units.placa.x + factorx, units.placa.y + factory, data.datos.placa)
       doc.setFontSize(10);
-      doc.text(data.datos.numeroMotor, 5.8+factorx,15.6+factory)
-      doc.setFontSize(12);
-      doc.text(data.datos.marca, 5.8+factorx,16.35+factory)
-      doc.text(data.datos.tarjetaOperacion, 5.8+factorx,17.08+factory)
-      doc.text(data.datos.numeroSOAT, 2.2+factorx,17.76+factory)
-      doc.setFontSize(12);
-      doc.text(data.datos.numeroRCC, 5.8+factorx,18.40+factory)
-      doc.text(data.datos.numeroRCE, 5.8+factorx,19.1+factory)
-      doc.text(data.datos.numeroTecnoMecanica, 5.8+factorx,19.81+factory)
-      doc.setFontSize(6);
-      doc.text(data.datos.nombreaseguradora, 9.4+factorx,17.76+factory)
-      doc.setFontSize(10);
-      doc.text(data.datos.numeroChasis, 13+factorx,15.6+factory)
-      doc.setFontSize(12);
-      doc.text(data.datos.modelo, 13+factorx,16.35+factory)
-      doc.text(data.datos.fechaTarjetaOperacionF, 14.6+factorx, 17.08+factory);
-      doc.text(data.datos.fechaNumeroSOAT, 14.6+factorx, 17.76+factory);
-      doc.text(data.datos.fechaNumeroRCC, 14.6+factorx, 18.38+factory); 
-      doc.text(data.datos.fechaNumeroRCE, 14.6+factorx, 19.08+factory); 
-      doc.text(data.datos.fechaNumeroTecnoMecanica, 14.6+factorx, 19.68+factory);   
 
-      doc.save('Tarjeton de conductor'+" "+ data.datos.nombres+' '+data.datos.apellidos);    
+
+      //MODIFICADO
+      doc.text(data.datos.consecutivo + " - " + data.datos.numeroMotor, units.numeroMotor.x + factorx, units.numeroMotor.y + factory)
+      doc.setFontSize(12);
+      doc.text(units.marca.x + factorx, units.marca.y + factory, data.datos.marca)
+      doc.text(units.tarjetaOperacion.x + factorx, units.tarjetaOperacion.y + factory, data.datos.tarjetaOperacion)
+      doc.text(units.numeroSOAT.x + factorx, units.numeroSOAT.y + factory, data.datos.numeroSOAT)
+      doc.setFontSize(12);
+      doc.text(units.numeroRCC.x + factorx, units.numeroRCC.y + factory, data.datos.numeroRCC)
+      doc.text(units.numeroRCE.x + factorx, units.numeroRCE.y + factory, data.datos.numeroRCE)
+      doc.text(units.numeroTecnoMecanica.x + factorx, units.numeroTecnoMecanica.y + factory, data.datos.numeroTecnoMecanica)
+
+
+      //MODIFICADO
+      doc.setFontSize(6);
+      doc.text(units.nombreaseguradora.x + factorx, units.nombreaseguradora.y + factory, data.datos.nombreaseguradora)
+      doc.setFontSize(10);
+
+      //MODIFICADO
+      doc.text(units.numeroChasis.x + factorx, units.numeroChasis.y + factory, data.datos.numeroChasis)
+      doc.setFontSize(12);
+      doc.text(units.modelo.x + factorx, units.modelo.y + factory, data.datos.modelo)
+
+      //MODIFICADO
+      doc.text(units.fechaTarjetaOperacionF.x + factorx, units.fechaTarjetaOperacionF.y + factory, data.datos.fechaTarjetaOperacionF);
+      doc.text(units.fechaNumeroSOAT.x + factorx, units.fechaNumeroSOAT.y + factory, data.datos.fechaNumeroSOAT);
+      doc.text(units.fechaNumeroRCC.x + factorx, units.fechaNumeroRCC.y + factory, data.datos.fechaNumeroRCC);
+      doc.text(units.fechaNumeroRCE.x + factorx, units.fechaNumeroRCE.y + factory, data.datos.fechaNumeroRCE);
+      doc.text(units.fechaNumeroTecnoMecanica.x + factorx, units.fechaNumeroTecnoMecanica.y + factory, data.datos.fechaNumeroTecnoMecanica);
+
+      doc.save('Tarjeton de conductor' + " " + data.datos.nombres + ' ' + data.datos.apellidos);
 
     };
-    this.loading.hide();
   }
 
 }
